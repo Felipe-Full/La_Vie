484bf5f0cf9496ac4263e11726c44465eb09e352
Felipe Barbosa <felipe_silviolobo@hotmail.com>
2023-03-17 20:50:32 -0300

    adicionado docs,controler,model

---
+++ src/controller/model/atendimentos.js
@@ -0,0 +1,44 @@
+const db = require("../database");
+const sequelize = require("sequelize");
+const Pacientes = require("./pacientes");
+const Psicologos = require("./psicologos");
+const Atendimentos = db.define(
+  "Atendimentos",
+  {
+    id: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    data_atendimento: {
+      type: sequelize.DATE,
+      allowNull: false
+    },
+    observacao: {
+      type: sequelize.STRING,
+      allowNull: false
+    },
+    id_paciente: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      Reference: {
+        model: Pacientes,
+        key: "id_paciente"
+      },
+    },
+    id_psicologo: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      Reference: {
+        model: Psicologos,
+        key: "id_psicologo"
+      },
+    },
+  },
+  {
+    tableName: "atendimentos",
+    underscored: true,
+    timestamps: false
+  }
+);
+module.exports = Atendimentos;
